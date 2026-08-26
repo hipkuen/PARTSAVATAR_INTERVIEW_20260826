44f85b0c9cdb5b7812dd72a112839e66f6a2ff59
hipkuen <chonghk1997@gmail.com>
2026-08-26 16:26:42 -0400

    Found out I just need to left join 2 times for task 8

--- sql_tasks/task_8_joins.py
+++ sql_tasks/task_8_joins.py
@@ -7,14 +7,18 @@ def get_customer_spend():
     # Task: Join Customers, Orders, and Order_Items to calculate 
     # total spend (price * quantity) per Customer Name.
     query = """
-    SELECT Customers.customer_id, Orders.order_id
+    SELECT Customers.customer_id, SUM(Order_Items.price*Order_Items.quantity)
     FROM Customers
     LEFT JOIN Orders
     ON Customers.customer_id = Orders.customer_id
+    LEFT JOIN Order_Items
+    ON Order_Items.order_id = Orders.order_id
+    GROUP BY Customers.customer_id
     """
     
     cursor.execute(query)
     results = cursor.fetchall()
     conn.close()
-    print(results)
     return results
+
+print(get_customer_spend())

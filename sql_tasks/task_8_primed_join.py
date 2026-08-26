87b8aa33e284df802c945518a82b631c62193f8e
hipkuen <chonghk1997@gmail.com>
2026-08-26 16:44:55 -0400

    Added task 8 primed for adding orders with shipped status

---
+++ sql_tasks/task_8_primed_join.py
@@ -0,0 +1,26 @@
+import sqlite3
+
+def get_customer_spend():
+    conn = sqlite3.connect('parts_avatar.db')
+    cursor = conn.cursor()
+    
+    # Task: Join Customers, Orders, and Order_Items to calculate 
+    # total spend (price * quantity) per Customer Name.
+    # Only calculate those orders with "Shipped" status
+    query = """
+    SELECT Customers.customer_id, SUM(Order_Items.price * Order_Items.quantity)
+    FROM Customers
+    LEFT JOIN Orders
+    ON Customers.customer_id = Orders.customer_id
+    LEFT JOIN Order_Items
+    ON Order_Items.order_id = Orders.order_id
+    WHERE Orders.status = 'Shipped'
+    GROUP BY Customers.customer_id
+    """
+    
+    cursor.execute(query)
+    results = cursor.fetchall()
+    conn.close()
+    return results
+
+print(get_customer_spend())
